4e3502c6215eb997339d5bb524a53208ec16bb1f
Chen Xiongjie <chen.xcmail@gmail.com>
2021-03-10 22:26:43 +0100

    Updated: to latest api

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,11 @@ import (
 
 	_ "github.com/digimakergo/dmdemo/entity"
 
+	_ "github.com/digimakergo/digimaker/core/auth/tokenmanager"
 	_ "github.com/digimakergo/digimaker/core/handler/handlers"
 	"github.com/digimakergo/digimaker/core/log"
 	"github.com/digimakergo/digimaker/core/util"
 	"github.com/digimakergo/digimaker/rest"
-	_ "github.com/digimakergo/digimaker/rest/tokenmanager"
 	"github.com/digimakergo/digimaker/sitekit"
 	"github.com/gorilla/mux"
 
@@ -25,21 +25,23 @@ func main() {
 	http.Handle("/var/", http.StripPrefix("/var/", http.FileServer(http.Dir(util.HomePath()+"/var"))))
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir(util.HomePath()+"/web/assets"))))
 
+	//Rest api
 	router := mux.NewRouter()
-
 	router.Use(rest.InitRequest)
-
-	//Route rest api
-	restRouter := router.PathPrefix("/api").Subrouter() //todo: make /api configuable
+	apiPrefix := "/api/"
+	restRouter := router.PathPrefix(apiPrefix).Subrouter() //todo: make /api configuable
 	rest.HandleRoute(restRouter)
+	http.Handle(apiPrefix, router)
 
-	//Handle content
-	siteRouters, defaultRouters := sitekit.GetSiteRouters(router)
+	//Website
+	siteRouter := mux.NewRouter()
+	siteRouter.Use(sitekit.InitRequest)
+	siteRouters, defaultRouters := sitekit.GetSiteRouters(siteRouter)
 
 	sitekit.RouteContent(siteRouters, defaultRouters)
 	log.Info("Sites content routered")
 
-	http.Handle("/", router)
+	http.Handle("/", siteRouter)
 
 	// go http.ListenAndServe(":9200", router)
 	go http.ListenAndServe(":9201", router)
