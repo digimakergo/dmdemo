6a4f0568e6be6d7b16bd88eb148db689431b2bc5
Chen Xiongjie <chen.xcmail@gmail.com>
2021-03-12 10:44:37 +0100

    Updated: style

--- web/app/src/App.tsx
+++ web/app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, BrowserRouter as Router, Route, Link, useLocation } from "react-router-dom";
+import { Switch, HashRouter as Router, Route, NavLink, Link, useLocation } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import DMInit from 'digimaker-ui/DMInit';
@@ -11,15 +11,15 @@ import ErrorBoundary from './ErrorBoundary';
 function App() {
   return (
     <div className="App">
-       <Router basename="/mypage">
+       <Router>
         <ErrorBoundary>
               <header className="App-header">
               <div>
               <h1><Link to="/">Mypage</Link></h1>
               <div className="right"><a href="/">Demo</a></div>
               <ul className="nav">
-                <li><Link to="/photos">Shared photos</Link> </li>
-                <li><Link to="/profile">Profile</Link> </li>
+                <li><NavLink activeClassName="active" to="/photos">Shared photos</NavLink> </li>
+                <li><NavLink activeClassName="active" to="/profile">Profile</NavLink> </li>
               </ul>
               </div>
               </header>
