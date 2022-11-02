189751ed42f4906d881ae380972f6384ea3aa5f1
Chen Xiongjie <chen.xcmail@gmail.com>
2022-11-02 21:34:11 +0100

    updated: clean up

--- web/admin/src/main/Main.tsx
+++ web/admin/src/main/Main.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import {
 	Link,
 } from "react-router-dom";
-import Moment from "react-moment";
 import List from "digimaker-ui/List";
 import { ContentActionParams } from "digimaker-ui/Actions";
 import MetaInfo from "./MetaInfo";
