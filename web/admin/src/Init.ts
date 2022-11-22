fa4ffc63cd791461b04e90e6e86c88156ebf9040
Chen Xiongjie <chen.xcmail@gmail.com>
2022-11-22 15:29:14 +0100

    udpated: remove editing article/summary, article/body for now

--- web/admin/src/Init.ts
+++
@@ -1,4 +0,0 @@
-import {FieldRegister} from 'digimaker-ui';
-import { FieldJson } from './fieldtype/FieldJson';
-
-FieldRegister.registerComponent( 'json', FieldJson );
---
+++ web/admin/src/Init.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {FieldRegister, FieldtypeProps} from 'digimaker-ui';
+import { FieldJson } from './fieldtype/FieldJson';
+
+
+FieldRegister.registerComponent( 'article/summary', ()=><div></div> );
+FieldRegister.registerComponent( 'article/body', ()=><div></div> );
+
+FieldRegister.registerComponent( 'json', FieldJson );
